ac9e7fd87a8c68b877ab7635f4138e5d21104a84
Joel Wasserman <joel.wasserman8@gmail.com>
2020-04-08 13:38:50 -0700

    added comment in index and fixed stripe bug

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const Process = require('./lib/process')
 
 const kms = new AWS.KMS({ region: 'us-west-2' })
 
+/** 
+ * Event contains an array of records to be processed.
+ * We have currently specified only 1 record to come in at a time.
+ * This is specified in the template.yml BatchSize field.
+ * the single record that comes in will have a body with a JSON stringified
+ * interface of:
+ * {
+ *   amount: int // amount that the advertiser owes in MICROCENTS, must be converted to cents for stripe
+ *   idempotencyKey: string // idempotent key for stripe to not make duplicate transactions
+ *   customerId: string // stripe customer id 
+ *   advertiserId: string // mongo id to update the mongo advertiser with their new balance
+ * }
+ */
+
 exports.handler = async (event) => {
   const config = new Config({ kms })
   const db = new Db({ config })
